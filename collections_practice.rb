874af815cf699f04bf326561a01a534bea2bde42
Anderson Coelho <anderson-tomas@hotmail.com>
2018-12-03 21:53:21 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,22 @@
 # your code goes here
+
+def begins_with_r(array)
+  firstLetter = []
+  sortedArray = []
+  counter = 0
+  truse = 0
+  while counter < array.length
+    firstLetter = array[counter].split("")
+    if firstLetter.first == "r"
+      truse += 1
+
+    end
+    counter+=1
+  end
+  if truse == array.length
+    return true
+  else
+    return false
+  end
+
+end
